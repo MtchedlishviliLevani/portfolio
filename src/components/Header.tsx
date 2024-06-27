dcc330ad77e0d439df68ee25206e9bb31980c7fb
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2024-06-27 23:29:08 +0400

    add some content on home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,9 @@ import { appStateContext } from "../helper/createContext";
 const Header = () => {
     const [listIndex, setListIndex] = useState<number>(0);
     const navigationList = ["Home", "About", "Projects", "Contact"];
+    console.log(listIndex)
 
-    const context = useContext(appStateContext)
+    const context = useContext(appStateContext);
 
     return (
         <header className="flex py-[35px] justify-between items-center">
@@ -25,35 +26,32 @@ const Header = () => {
             <AnimatePresence>
                 {context?.isHiddenNavigation && (
                     <motion.ul
-
                         initial={{ left: "2500px" }}
                         animate={{ left: "0" }}
                         transition={{ duration: 1.5 }}
                         exit={{ left: "-2500px", transition: { duration: 1.5 } }}
-                        className={` flex flex-col  fixed top-[130px] w-[100%] md:min-w-[400px] md:w-[60%] md:left-[initial] md:static md:flex-row md:justify-between`}
+                        className={` flex flex-col  fixed top-[130px] w-[100%] bg-[#06192F] h-[calc(100vh-122px)] md:h-[initial] md:min-w-[400px] md:w-[60%] md:left-[initial] md:static md:flex-row md:justify-between`}
                     >
                         {navigationList.map((value, i) => (
-                            <motion.li
-
+                            <li
                                 key={i}
                                 onClick={() => setListIndex(i)}
                                 className={`${listIndex === i
-                                    ? "bg-[#c82727] md:bg-transparent md:text-red-400 delay-75 btn"
-                                    : "border-solid border-b md:border-b-0  border-white md:border-transparent"
+                                    ? "bg-[#c82727]  md:text-red-400 md:font-bold delay-75 md:delay-0 btn"
+                                    : "border-solid border-b md:border-b-0  border-white md:border-transparent  "
                                     }
                                      ${listIndex - 1 === i &&
                                     "border-solid md:border-[initial] border-b-transparent border-transparent"
                                     } 
-                                    cursor-pointer  text-[24px] text-center md:text-left text-white p-[25px] md:p-0 md:items-center`}
-
+                                    cursor-pointer  text-[24px] text-center md:text-left text-white p-[25px] md:p-0 md:py-[8px] md:items-center relative after:content-[''] after:absolute after:w-[100%] after:h-[1px] after:bg-white  after:left-[0%] after:opacity-0 hover:after:opacity-100 after:bottom-0 after:duration-[1s] after:transition-[opacity]`}
                             >
                                 {value}
-                            </motion.li>
+                            </li>
                         ))}
                     </motion.ul>
                 )}
             </AnimatePresence>
-        </header >
+        </header>
     );
 };
 
--- src/components/MainComp.tsx
+++ src/components/MainComp.tsx
@@ -1,10 +1,13 @@
 import { motion } from "framer-motion"
 import Header from "./Header"
+import MainContent from "./MainContent"
 function MainComp() {
     return (
         <motion.div initial={{ left: "101%" }} animate={{ left: 0 }} transition={{ delay: 2.5, duration: 1.3 }} className='w-[100%] h-[100vh] bg-bgColor absolute z-40'>
             <div className="container font-jura">
-                <Header /> </div>
+                <Header />
+                <MainContent />
+            </div>
         </motion.div>
     )
 }
